cc1c00942acae19f687c7633b92c8920f57b4a7d
Mohieddine-Al-Hariri <114818981+Mohieddine-Al-Hariri@users.noreply.github.com>
2022-10-02 18:42:09 +0300

    .

---
+++ projJS.js
@@ -0,0 +1,8 @@
+// const lins = document.getElementsByClassName('link');
+// $(function(){
+//     $("#select a").click(function(){
+//         $("#select a").removeClass("link")
+//         $(this).addClass("pressed")
+//         return false;
+//     })
+// })
